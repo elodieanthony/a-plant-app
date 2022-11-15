49b2c873cc437637812896fe99fbbfce866f55f6
Elodie <anthonyelodie@gmail.com>
2022-11-15 12:42:50 +0100

    starting project

---
+++ src/components/navigation/MainNavigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import MainHeader from '../navigation/MainHeader';
+
+import './MainNavigation.css';
+
+const MainNavigation = () => {
+  return <MainHeader></MainHeader>;
+};
+
+export default MainNavigation;
