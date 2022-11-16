510ce3bbb6acf5653526a4e9831110e223abbb06
Elodie <anthonyelodie@gmail.com>
2022-11-16 14:42:09 +0100

    replace href by Link

--- src/components/navigation/NavLinks.js
+++ src/components/navigation/NavLinks.js
@@ -1,16 +1,19 @@
+import { Link } from 'react-router-dom';
+
 import './NavLinks.css';
 
-const NavLinks = () => {
+const NavLinks = props => {
+  //Add proper user id once context is created
   return (
     <ul className='nav-links'>
       <li>
-        <a href='/'>My plants</a>
+        <Link to='/userId/plants'>My plants</Link>
       </li>
       <li>
-        <a href='/plants/new'>Add plants</a>
+        <Link to='/plants/new'>Add plants</Link>
       </li>
       <li>
-        <a href='/auth'>Login</a>
+        <Link to='/auth'>Login</Link>
       </li>
     </ul>
   );
