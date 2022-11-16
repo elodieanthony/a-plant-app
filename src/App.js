1e9ce58ded90b3a5cd10cd60df94cce0f03cffe6
Elodie <anthonyelodie@gmail.com>
2022-11-16 14:41:24 +0100

    Add router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import MainNavigation from './components/navigation/MainNavigation';
+import Welcome from './pages/Welcome';
+import Auth from './pages/Auth';
+import UsersPlants from './pages/UsersPlants';
 
 import './App.css';
 
 function App() {
   const content = (
-    <main>
+    <Router>
       <MainNavigation />
-    </main>
+      <main>
+        <Routes>
+          <Route path='/' element={<Welcome />}></Route>
+          <Route path='/auth' element={<Auth />}></Route>
+          <Route path='/:userId/plants' element={<UsersPlants />}></Route>
+        </Routes>
+      </main>
+    </Router>
   );
 
   return ReactDOM.createPortal(content, document.getElementById('drawer-hook'));
