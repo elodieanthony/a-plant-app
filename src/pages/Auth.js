c6a7780db7e923c4f4387bd8956f10e6f4f0559d
Elodie <anthonyelodie@gmail.com>
2022-11-16 15:17:36 +0100

    add basic authentiaction

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,5 +1,33 @@
+import React, { useState } from 'react';
+
+import Card from '../components/UIElements/Card';
+import Input from '../components/formElements/Input';
+import Button from '../components/formElements/Button';
+
+import './Auth.css';
+
 const Auth = () => {
-  return <h1>Authenticate page</h1>;
+  const [isLoginMode, setIsLoginMode] = useState(true);
+
+  const switchModeHandler = () => {
+    setIsLoginMode(prevMode => !prevMode);
+  };
+
+  return (
+    <Card className='authentication'>
+      <h2>Login</h2>
+      <hr />
+      {!isLoginMode && <Input element='input' id='name' label='Name'></Input>}
+
+      <Input element='input' id='email' type='email' label='Email' />
+      <Input type='password' element='input' id='password' label='Password' />
+      <Button type='submit'>{isLoginMode ? 'login' : 'SIGNUP'}</Button>
+      {isLoginMode && <p>Not registered yet?</p>}
+      <Button inverse onClick={switchModeHandler}>
+        SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
+      </Button>
+    </Card>
+  );
 };
 
 export default Auth;
