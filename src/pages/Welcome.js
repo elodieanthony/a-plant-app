0addb7b5d3993da307990f8382f4f724483bff04
Elodie <anthonyelodie@gmail.com>
2022-11-16 14:39:50 +0100

    update welcome page

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,8 +1,18 @@
+import Button from '../components/UIElements/Button';
+import './Welcome.css';
+
 const Welcome = () => {
   return (
-    <div>
+    <div className='welcome-page'>
       <h1>Welcome on a Plant App! </h1>
-      <p>In this place you can collect and reference all the plants you own.</p>
+      <p>In here you can collect and reference all the plants you own.</p>
+      <p>And more!</p>
+      <p>Don't hesitate to join us by</p>
+      <div className='align-content'>
+        <Button inverse>Subscribe</Button>
+        <p>Or</p>
+        <Button>Login</Button>
+      </div>
     </div>
   );
 };
