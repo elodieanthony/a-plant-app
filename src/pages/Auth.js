072fcb508f0627e0f0071399568989067e7b6d20
Elodie <anthonyelodie@gmail.com>
2022-11-17 11:36:17 +0100

    add validators and reducer for basic input validation

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -3,6 +3,11 @@ import React, { useState } from 'react';
 import Card from '../components/UIElements/Card';
 import Input from '../components/formElements/Input';
 import Button from '../components/formElements/Button';
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from '../util/validators';
 
 import './Auth.css';
 
@@ -15,12 +20,34 @@ const Auth = () => {
 
   return (
     <Card className='authentication'>
-      <h2>Login</h2>
+      {isLoginMode ? <h2>Login</h2> : <h2>Signup</h2>}
       <hr />
-      {!isLoginMode && <Input element='input' id='name' label='Name'></Input>}
+      {!isLoginMode && (
+        <Input
+          element='input'
+          id='name'
+          label='Name'
+          errorText='Please enter a valid name.'
+          validators={[VALIDATOR_REQUIRE()]}
+        ></Input>
+      )}
 
-      <Input element='input' id='email' type='email' label='Email' />
-      <Input type='password' element='input' id='password' label='Password' />
+      <Input
+        element='input'
+        id='email'
+        type='email'
+        label='Email'
+        errorText='Please enter a valid email address.'
+        validators={[VALIDATOR_REQUIRE()]}
+      />
+      <Input
+        type='password'
+        element='input'
+        id='password'
+        label='Password'
+        errorText='Please enter a valid password (at lease 6 characters)'
+        validators={[VALIDATOR_MINLENGTH(6)]}
+      />
       <Button type='submit'>{isLoginMode ? 'login' : 'SIGNUP'}</Button>
       {isLoginMode && <p>Not registered yet?</p>}
       <Button inverse onClick={switchModeHandler}>
