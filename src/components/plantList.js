ea52e6879b0ac00408fd5d2df899bf0cc4ceddd2
Elodie <anthonyelodie@gmail.com>
2022-11-21 12:12:55 +0100

    replace custom css with masonry

--- src/components/plantList.js
+++ src/components/plantList.js
@@ -1,26 +1,38 @@
+import Masonry from 'react-masonry-css';
+
 import PlantItem from './PlantItem';
+
 import './PlantList.css';
 
 const PlantList = props => {
+  const breakpointColumnsObj = {
+    default: 3,
+    800: 2,
+    500: 1,
+  };
   return (
     <ul className='plant-list'>
-      {props.items.map(plant => (
-        <PlantItem
-          key={plant.id}
-          id={plant.id}
-          image={plant.image}
-          name={plant.name}
-          description={plant.description}
-          water={plant.water}
-          fertilizer={plant.fertilizer}
-          expo={plant.expo}
-          soil={plant.soil}
-          temperature={plant.temperature}
-          repoting={plant.repoting}
-        />
-      ))}
-      <span className='plant-item break'></span>
-      <span className='plant-item break'></span>
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className='grid'
+        columnClassName='column'
+      >
+        {props.items.map(plant => (
+          <PlantItem
+            key={plant.id}
+            id={plant.id}
+            image={plant.image}
+            name={plant.name}
+            description={plant.description}
+            water={plant.water}
+            fertilizer={plant.fertilizer}
+            expo={plant.expo}
+            soil={plant.soil}
+            temperature={plant.temperature}
+            repoting={plant.repoting}
+          />
+        ))}
+      </Masonry>
     </ul>
   );
 };
