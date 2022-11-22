2d5503da93dc8aa272c5990f1e8934a0859cdbe6
Elodie <anthonyelodie@gmail.com>
2022-11-22 14:06:40 +0100

    add basic calendar

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,5 +1,22 @@
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+
+import 'react-calendar/dist/Calendar.css';
+import './CustomCalendar.css';
+
 const Canlendar = props => {
-    return <h1>Calendar</h1>
+  const [value, onChange] = useState(new Date());
+  return (
+    <div className='calendar-container center'>
+      <Calendar
+        onChange={onChange}
+        value={value}
+        className='calendar'
+        defaultView='week'
+        // formatDay={(locale, date) => formatDate(date, 'd')}
+      />
+    </div>
+  );
 };
 
 export default Canlendar;
