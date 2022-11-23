8711df587dee4c7da2fc8780b771c52487bc1b77
Elodie <anthonyelodie@gmail.com>
2022-11-23 16:07:40 +0100

    Change for a calendar with events handler

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,23 +1,95 @@
-import { useState } from 'react';
-import Calendar from 'react-calendar';
+import { Fragment, useState } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
 
-import 'react-calendar/dist/Calendar.css';
+import Button from '../components/formElements/Button';
+import Modal from '../components/UIElements/Modal';
+import Input from '../components/formElements/Input';
+import { useForm } from '../hooks/form-hook';
+
+// import 'react-calendar/dist/Calendar.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 import './CustomCalendar.css';
+import { VALIDATOR_REQUIRE } from '../util/validators';
 
 const Canlendar = props => {
-  const [value, onChange] = useState(new Date());
+  const [showModal, setShowModal] = useState(false);
+  const [formState, inputHandler] = useForm({
+    name: { value: '', isValid: false },
+    wateringTime: { value: '', isValid: false },
+  });
+
+  const localizer = momentLocalizer(moment);
+  const addNewEventHandler = () => {
+    setShowModal(true);
+  };
+
+  const cancelAddNewEventhandler = () => {
+    setShowModal(false);
+  };
+
+  const addNewEventSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState);
+  };
+
   return (
-    <div className='calendar-container center'>
-      <Calendar
-        onChange={onChange}
-        value={value}
-        className='calendar'
-        defaultView='week'
-        onClickDay={(value, event) => {
-          alert('Clicked day:', value);
-        }}
-      />
-    </div>
+    <Fragment>
+      <Modal
+        show={showModal}
+        onCancel={cancelAddNewEventhandler}
+        header='Add new Event'
+        footerClass='plant-item__modal-actions'
+        footer={
+          <Fragment>
+            <Button inverse type='submit' disabled={!formState.isValid}>
+              CONFIRM
+            </Button>
+            <Button danger onClick={cancelAddNewEventhandler}>
+              CANCEL
+            </Button>
+          </Fragment>
+        }
+        onSubmit={addNewEventSubmitHandler}
+      >
+        <Input
+          type='text'
+          id='name'
+          element='input'
+          label='Plant name'
+          validators={[VALIDATOR_REQUIRE]}
+          errorText='Please enter a valid name.'
+          onInput={inputHandler}
+        ></Input>
+        <Input
+          type='text'
+          id='watering-time'
+          element='input'
+          label='Number of watering by week'
+          validators={[]}
+          errorText='Please enter a valid number.'
+          onInput={inputHandler}
+        ></Input>
+      </Modal>
+      <Button onClick={addNewEventHandler}>Add New Event</Button>
+      {/* <div className='calendar-container center'>
+        <Calendar
+          onChange={onChange}
+          value={value}
+          className='calendar'
+          onClickDay={(value, event) => {
+            alert('Clicked day:', value);
+          }}
+        />
+      </div> */}
+      <div className='calendar-container'>
+        <Calendar
+          localizer={localizer}
+          startAccessor='start'
+          endAccessor='end'
+        />
+      </div>
+    </Fragment>
   );
 };
 
